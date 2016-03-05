d87ea1819fff4a7026ed43402e838fd41aa87f05
PatriciaPinilla <claupinilla@uan.edu.co>
2016-03-05 08:29:31 -0500

    Segundo Intento

--- app/src/main/java/com/example/winchester/proyecto/MainActivity.java
+++ app/src/main/java/com/example/winchester/proyecto/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.winchester.proyecto;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registrarEventos();
+    }
+    private void registrarEventos (){
+        ListView parques = (ListView) findViewById(R.id.NombreParque);
+
+        parques.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String itemSeleccionado = adapterView.getItemAtPosition(i).toString();
+
+                Toast.makeText(getApplicationContext(), "Haz hecho click en " + itemSeleccionado, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.NombreParque){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
